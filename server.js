43dabe4f2f793a1d6a295ca9e8c7dd78d316e022
Frederick Rosales <frederick.rosales@gmail.com>
2014-05-26 21:57:25 +0400

    first Commit

---
+++ server.js
@@ -0,0 +1,12 @@
+
+var http = require("http"),
+    port = process.env.PORT || 1881;  
+ 
+var server = http.createServer(function(request,response){  
+    response.writeHeader(200, {"Content-Type": "text/plain"});  
+    response.write("Hello HTTP yes Riki updated this!");  
+    response.end();  
+});
+ 
+server.listen(port);  
+console.log("Server Running on "+port+".\nLaunch http://localhost:"+port);
